d739925c4ded1a18170747d182f2f7d82fc6e86f
matthiaskaeding <kaedingmatthias@gmail.com>
2024-12-07 19:49:01 +0100

    Wrong solution

--- day06/utils.go
+++ day06/utils.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 // Data model:
@@ -19,28 +20,48 @@ type GuardPosition [2]int
 
 type Direction string
 
+func (d Direction) Delta() (int, int, error) {
+	switch d {
+	case "u":
+		return -1, 0, nil
+	case "d":
+		return 1, 0, nil
+	case "l":
+		return 0, -1, nil
+	case "r":
+		return 0, 1, nil
+	default:
+		return 0, 0, fmt.Errorf("wrong value of direction, got %v, must be u, d,l, r", d)
+	}
+}
+
 func (d Direction) Change() (Direction, error) {
 	switch d {
 	case "u":
 		return Direction("r"), nil
+	case "r":
+		return Direction("d"), nil
 	case "d":
 		return Direction("l"), nil
 	case "l":
 		return Direction("u"), nil
-	case "r":
-		return Direction("d"), nil
 	default:
-		return Direction(""), fmt.Errorf("input directionst be be in u,d,l,r but is %v", d)
+		return Direction(""), fmt.Errorf("input direction must be be in u,d,l,r but is %v", d)
 	}
-
 }
 
+type Dims struct {
+	numRows int
+	numCols int
+}
+type VisitedLocations map[GuardPosition]bool
+
 type GuardMap struct {
-	obstacles     ObstacleMatrix
-	direction     Direction
-	guardPosition GuardPosition
-	n             int
-	k             int
+	obstacles        ObstacleMatrix
+	direction        Direction
+	guardPosition    GuardPosition
+	dims             Dims
+	visitedLocations VisitedLocations
 }
 
 func (g GuardMap) String() string {
@@ -90,38 +111,34 @@ func ReadInput(file string) (GuardMap, error) {
 	if direction == "" {
 		return GuardMap{}, fmt.Errorf("direction not set")
 	}
-	n := len(obstacles)
-	k := len(obstacles[0])
-	return GuardMap{obstacles, direction, guardPosition, n, k}, nil
+	dims := Dims{len(obstacles), len(obstacles[0])}
+	visitedLocations := make(VisitedLocations)
+	visitedLocations[guardPosition] = true
+	return GuardMap{obstacles, direction, guardPosition, dims, visitedLocations}, nil
 }
 
-func makeDeltaDirection(d Direction) (int, int, error) {
-	var di, dj int
-	switch d {
-	case "u":
-		di = -1 // Row up
-	case "d":
-		di = 1 // Row down
-	case "l":
-		dj = -1 // Col left
-	case "r":
-		dj = 1 // Col right
-	default:
-		return di, dj, fmt.Errorf("wrong value of direction, got %v, must be u, d,l, r", d)
-	}
-	return di, dj, nil
-}
-
-func (guardMap GuardMap) Print(i ...int) {
+func (guardMap *GuardMap) Print(i int) {
 	var maxLines int
-	if i[0] > 0 {
-		maxLines = i[0]
+	if i == -1 {
+		maxLines = guardMap.dims.numRows
 	} else {
-		maxLines = guardMap.n
+		maxLines = i
 	}
+	s := "  "
+	numS := ""
+	for i := 0; i < guardMap.dims.numCols; i++ {
+		numS += strconv.Itoa(i)
+	}
+	s += numS + "\n"
 	for i := 0; i < maxLines; i++ {
-		fmt.Println(guardMap.obstacles[i])
+		s += strconv.Itoa(i) + " "
+		for _, j := range guardMap.obstacles[i] {
+			s += j
+		}
+		s += strconv.Itoa(i) + "\n"
 	}
+	s += "  " + numS
+	fmt.Println(s)
 }
 
 func (guardMap *GuardMap) moveGuard() (bool, error) {
@@ -130,7 +147,7 @@ func (guardMap *GuardMap) moveGuard() (bool, error) {
 	)
 	gi := guardMap.guardPosition[0]
 	gj := guardMap.guardPosition[1]
-	di, dj, err := makeDeltaDirection(guardMap.direction)
+	di, dj, err := guardMap.direction.Delta()
 	if err != nil {
 		return guardIsFree, err
 	}
@@ -138,7 +155,12 @@ func (guardMap *GuardMap) moveGuard() (bool, error) {
 	for !hitObstacle {
 		gi = gi + di
 		gj = gj + dj
-		newPosition := GuardPosition{gi, gj}
+		fmt.Printf("gi: %v. gj: %v\n", gi, gj)
+		// Check if the poor guard is free
+		if gi < 0 || gi == guardMap.dims.numRows || gj < 0 || gj == guardMap.dims.numCols {
+			fmt.Printf("Final position: %v %v\n", gi, gj)
+			return true, nil
+		}
 		hitObstacle = guardMap.obstacles[gi][gj] == "#"
 		if hitObstacle { // Redo moving and change direction
 			gi = gi - di
@@ -148,21 +170,27 @@ func (guardMap *GuardMap) moveGuard() (bool, error) {
 				return false, err
 			}
 			guardMap.direction = newDirection
-			guardMap.obstacles[gi][gj] = string(newDirection)
-		} else { // Only move when we dont hit an obstacle
-
-			guardMap.obstacles[guardMap.guardPosition[0]][guardMap.guardPosition[1]] = "."
-			guardMap.guardPosition = newPosition
 		}
-
-		fmt.Printf("gi: %v. gj: %v. Hit obstacle: %v. Direction: %v \n",
-			gi, gj, hitObstacle, guardMap.direction)
-		// Check if the poor guard is free
-		if gi < 0 || gi == guardMap.n || gj < 0 || gj == guardMap.k {
-			guardIsFree = true
-		}
+		guardMap.obstacles[gi][gj] = "X"
+		guardMap.guardPosition = GuardPosition{gi, gj}
+		guardMap.visitedLocations[guardMap.guardPosition] = true
 	}
 	return guardIsFree, nil
+}
+
+func (guardMap *GuardMap) FreeGuard() (int, error) {
+	var (
+		numSteps int
+		isFree   bool
+	)
+	for !isFree {
+		isFreeI, err := guardMap.moveGuard()
+		if err != nil {
+			return numSteps, err
+		}
+		isFree = isFreeI
+	}
+	return len(guardMap.visitedLocations), nil
 }
 
 func Solve() {
@@ -174,16 +202,31 @@ func Solve() {
 	fmt.Println(guardMap.direction)
 	fmt.Println(" ")
 	fmt.Println(guardMap.direction)
-	guardMap.moveGuard()
-	fmt.Println(guardMap.direction)
 
-	for i := 0; i < 3; i++ {
-		fmt.Println(guardMap.obstacles[i])
-	}
+	guardMap.Print(5)
 	guardMap.moveGuard()
-	for i := 0; i < 3; i++ {
-		fmt.Println(guardMap.obstacles[i])
+	guardMap.Print(5)
+	guardMap.moveGuard()
+	guardMap.Print(10)
+	guardMap.moveGuard()
+	guardMap.Print(10)
+
+	numSteps, err := guardMap.FreeGuard()
+	if err != nil {
+		fmt.Print(err)
 	}
+	guardMap.Print(10)
+
+	n2 := 0
+	for i := 0; i < guardMap.dims.numRows; i++ {
+		for j := 0; j < guardMap.dims.numCols; j++ {
+			if guardMap.obstacles[i][j] == "X" {
+				n2 += 1
+			}
+		}
+	}
+
+	fmt.Printf("Number of steps: %v, n2 = %v\n", numSteps, n2)
 
 	// Do something
 }
