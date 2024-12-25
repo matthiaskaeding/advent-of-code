10a6d14b64e195945b4298847e871857fbfb8eec
Matthias Kaeding <kaedingmatthias@gmail.com>
2024-12-25 11:50:29 +0100

    Solved (#15)
    * Start with files
    
    * Example solved
    
    * Solved

--- day09/utils.go
+++ day09/utils.go
@@ -7,6 +7,11 @@ import (
 	"strconv"
 )
 
+type interval struct {
+	lower int
+	upper int
+}
+
 func ReadInput(file string) string {
 
 	var lines []string
@@ -31,29 +36,42 @@ func ReadInput(file string) string {
 
 }
 
-type Disc []int
+type Disc struct {
+	data  []int
+	sizes map[int]int // Maps file id to file size
+}
+
+func (d Disc) Len() int {
+	return len(d.data)
+}
+func (d Disc) At(i int) int {
+	return d.data[i]
+}
 
 func MakeDisc(s string) Disc {
-	var disc Disc
+
+	var data []int
+	sizes := make(map[int]int)
 	var id, right int
 	for i, r := range s {
 		blockSize := int(r - '0')
 		if i%2 == 0 {
 			right = id
+			sizes[id] = blockSize
 			id++
 		} else {
 			right = -1
 		}
 		for j := 0; j < blockSize; j++ {
-			disc = append(disc, right)
+			data = append(data, right)
 		}
 	}
-	return disc
+	return Disc{data, sizes}
 }
 
 func (d Disc) Show() {
 	s := ""
-	for _, x := range d {
+	for _, x := range d.data {
 		if x == -1 {
 			s += "."
 		} else {
@@ -65,73 +83,76 @@ func (d Disc) Show() {
 }
 
 // Gives the indices [low, up] for first space
-func (d Disc) FindSpace() (int, int, bool) {
-	u := -1
-	for i := range d {
-		for j := i; j < len(d) && d[j] == -1; j++ {
-			u = j
-		}
-		if u != -1 {
-			return i, u, true
+func (d Disc) FindSpace(size int) (interval, bool) {
+	for l := range d.data {
+		for j := l; j < d.Len() && d.At(j) == -1; j++ {
+			u := j
+			if u-l+1 == size {
+				return interval{l, u}, true
+			}
 		}
 	}
-	return 0, 0, false
+	return interval{}, false
 }
 
 func IsNumber(r byte) bool {
 	return r >= '0' && r <= '9'
 }
 
-func (d Disc) GetReplacement(n int, u int) ([]int, []int) {
-	indices := []int{}
-	replacements := []int{}
-	for i := u; i >= 0 && len(indices) < n; i-- {
-		b := d[i]
-		if b >= 0 {
-			indices = append(indices, i)
-			replacements = append(replacements, b)
+// Gets the replacement. For all files where we dont find space,
+// we delete the entry in d.sizes, so that we will skip those files in the future
+// Returns: replacement value, lower and upper position of space
+func (d *Disc) GetReplacement() (interval, interval, int, bool) {
+	for i := d.Len() - 1; i >= 0; i-- {
+		b := d.At(i)
+		if b == -1 {
+			continue
 		}
+		bSize, exists := d.sizes[b]
+		if !exists {
+			continue
+		}
+		intervalSpace, foundSpace := d.FindSpace(bSize)
+		if !foundSpace || intervalSpace.lower > i {
+			delete(d.sizes, b)
+			continue
+		}
+		// i = 10, bsize = 1: i + 1 - 1 = 10 correct
+		// i = 10, bsize = 2: i - 2 + 1  = 9  correct
+		return intervalSpace, interval{i - bSize + 1, i}, b, true
 	}
-	return indices, replacements
+	return interval{}, interval{}, 0, false
 }
 
-func (d Disc) Move() (Disc, bool) {
-	l, u, found := d.FindSpace()
-	var (
-		indices []int
-	)
-	if found {
-		indices, _ = d.GetReplacement(u-l+1, len(d)-1)
+func (d *Disc) Move() bool {
+	intervalSpace, intervalRepl, repl, replFound := d.GetReplacement()
+	if !replFound {
+		return false
 	}
 
-	// Create a new slice with its own underlying array
-	db := make([]int, len(d))
-	copy(db, d) // Copy the elements from d to db
+	intervalSize := intervalSpace.upper - intervalSpace.lower + 1
+	for i := 0; i < intervalSize; i++ {
+		d.data[intervalSpace.lower+i] = repl
+		d.data[intervalRepl.lower+i] = -1
 
-	for i := l; i <= u; i++ {
-		posReplacement := indices[i-l]
-		if posReplacement < l {
-			return Disc(db), true
-		}
-		db[i] = db[posReplacement]
-		db[posReplacement] = -1
 	}
-	return Disc(db), false
+
+	return true
 }
 
 func (d Disc) MoveAll() Disc {
-	d, done := d.Move()
-	for !done {
-		d, done = d.Move()
+	done := d.Move()
+	for done {
+		done = d.Move()
 	}
 	return d
 }
 
 func (d Disc) CompCheckSum() int {
 	checkSum := 0
-	for i, x := range d {
+	for i, x := range d.data {
 		if x == -1 {
-			break
+			continue
 		}
 		checkSum += x * i
 	}
@@ -144,9 +165,10 @@ func Solve() {
 
 	fmt.Println()
 	disc.Show()
-	discMoved := disc.MoveAll()
-	discMoved.Show()
-	checkSum := discMoved.CompCheckSum()
+
+	disc.MoveAll()
+	disc.Show()
+	checkSum := disc.CompCheckSum()
 	fmt.Printf("Checksum: %v\n", checkSum)
 
 }
